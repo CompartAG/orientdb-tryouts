408d24384f11430091779fde4736b5ae7e9781e8
Francois Charette <firmicus@ankabut.net>
2016-04-15 14:23:00 +0200

    Add test to check that graph.commit is not possible after graph.shutdown

---
+++ src/test/java/com/compart/tec/GraphShutdownTest.java
@@ -0,0 +1,25 @@
+package com.compart.tec;
+
+import com.orientechnologies.orient.core.exception.ODatabaseException;
+import com.tinkerpop.blueprints.impls.orient.OrientConfigurableGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientGraph;
+import com.tinkerpop.blueprints.impls.orient.OrientGraphFactory;
+import org.junit.Assert;
+import org.junit.Test;
+
+/**
+ * Created by fc7 on 14/04/2016.
+ */
+public class GraphShutdownTest {
+
+    @Test (expected = ODatabaseException.class)
+    public void testShutdownGraph_graphCommitAfterShutdown() {
+        OrientGraphFactory factory = new OrientGraphFactory("memory:test");
+        OrientGraph graph1 = factory.getTx();
+        graph1.setThreadMode(OrientConfigurableGraph.THREAD_MODE.ALWAYS_AUTOSET);
+        OrientGraph graph2 = factory.getTx();
+        graph2.setThreadMode(OrientConfigurableGraph.THREAD_MODE.ALWAYS_AUTOSET);
+        graph2.shutdown(true); // in 2.2 this will not close the database because graph1 is still open
+        graph2.commit(); // this should fail
+    }
+}
