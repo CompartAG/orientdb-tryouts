0f90484ed448ca6f108f694e85c5221e6be2c4e2
diegomtassis <diego.mt.dev@gmail.com>
2018-05-17 18:58:56 +0200

    fixed javadoc again

--- 2.2.x/src/test/java/com/compart/tec/orientdb/object/PropagateSaveOnSetFieldIT.java
+++ 2.2.x/src/test/java/com/compart/tec/orientdb/object/PropagateSaveOnSetFieldIT.java
@@ -45,36 +45,6 @@ public class PropagateSaveOnSetFieldIT extends AbstractOrientDBObjectITest {
     }
 
     /**
-     * Propagating a save operation over a Collection field (and then reading) using different connections. It works
-     */
-    @Test
-    public void testPropagateSaveOverCollection_DifferentConnections() {
-
-        // setup: infrastructure
-        registerEntities();
-
-        // setup: data
-        Man ayrton = new Man();
-        Man alain = new Man();
-        ayrton.addEnemy(alain);
-        alain.setFavoriteFood("raclette");
-        Man savedAyrton = this.getDatabase().save(ayrton);
-        String ayrtonId = savedAyrton.getId();
-
-        // exercise
-        this.releaseCurrentConnection();
-        Person retrievedAyrton = this.getDatabase().load(new ORecordId(ayrtonId));
-        Person retrievedAlain = retrievedAyrton.getEnemies().iterator().next();
-        retrievedAlain.setFavoriteFood("scargots");
-        savedAyrton = this.getDatabase().save(retrievedAyrton);
-
-        // verify
-        this.releaseCurrentConnection();
-        retrievedAyrton = this.getDatabase().load(new ORecordId(ayrtonId));
-        Assert.assertEquals("scargots", retrievedAyrton.getEnemies().iterator().next().getFavoriteFood());
-    }
-    
-    /**
      * Propagating a save operation over a Set field (and then reading) using different connections. It fails.
      */
     @Test
@@ -102,6 +72,36 @@ public class PropagateSaveOnSetFieldIT extends AbstractOrientDBObjectITest {
         this.releaseCurrentConnection();
         retrievedAyrton = this.getDatabase().load(new ORecordId(ayrtonId));
         Assert.assertEquals("scargots", retrievedAyrton.getFriends().iterator().next().getFavoriteFood());
+    }
+    
+    /**
+     * Propagating a save operation over a Collection field (and then reading) using different connections. It works
+     */
+    @Test
+    public void testPropagateSaveOverCollection_DifferentConnections() {
+
+        // setup: infrastructure
+        registerEntities();
+
+        // setup: data
+        Man ayrton = new Man();
+        Man alain = new Man();
+        ayrton.addEnemy(alain);
+        alain.setFavoriteFood("raclette");
+        Man savedAyrton = this.getDatabase().save(ayrton);
+        String ayrtonId = savedAyrton.getId();
+
+        // exercise
+        this.releaseCurrentConnection();
+        Person retrievedAyrton = this.getDatabase().load(new ORecordId(ayrtonId));
+        Person retrievedAlain = retrievedAyrton.getEnemies().iterator().next();
+        retrievedAlain.setFavoriteFood("scargots");
+        savedAyrton = this.getDatabase().save(retrievedAyrton);
+
+        // verify
+        this.releaseCurrentConnection();
+        retrievedAyrton = this.getDatabase().load(new ORecordId(ayrtonId));
+        Assert.assertEquals("scargots", retrievedAyrton.getEnemies().iterator().next().getFavoriteFood());
     }
 
     private void registerEntities() {
